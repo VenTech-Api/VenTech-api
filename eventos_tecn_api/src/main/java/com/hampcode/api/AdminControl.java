e9f4035da2f91dae9860d2012445bb50b4f154b1
VasquezCastilloJherson <jvasquezc27@upao.edu.pe>
2024-09-30 17:32:21 -0500

    Configuracion de la estructura inicial del proyecto

---
+++ eventos_tecn_api/src/main/java/com/hampcode/api/AdminControl.java
@@ -0,0 +1,7 @@
+package com.hampcode.api;
+
+public class AdminControl {
+    void hello(){
+        System.out.println("hello");
+    }
+}
---
+++ eventos_tecn_api/src/main/java/com/hampcode/repository/UsuariosRepository.java
@@ -0,0 +1,5 @@
+package com.hampcode.repository;
+
+public interface UsuariosRepository {
+    void hello();
+}
